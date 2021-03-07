12bf0c933cc2092b01373bb9ab167c067d559799
Modnox <marco.feder@gmx.de>
2021-03-07 20:05:23 +0100

    initial commit

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ module.exports = function pepega(dispatch) {
     let names = ['Keiru']
     let messages =
         [': Another One!',
-         'you TRASH LMAO.',
-         '. Here we go again!',
-         'PEPEGA!'
+            ' you TRASH LMAO.',
+            '. Here we go again!',
+            ' PEPEGA!',
+            ' nice floormat.',
+            ' is getting closer to the wheelchair.'
         ]
 
     dispatch.command.add('pepega', (cmd) => {
         switch (cmd) {
             case "rng":
-                sendMessage()
+                sendMessage('Test')
                 break;
             default:
                 enabled = !enabled
@@ -20,14 +22,14 @@ module.exports = function pepega(dispatch) {
     })
 
     dispatch.hook('S_USER_DEATH', 1, (event) => {
-       if(names.includes(event.name)){
-           console.log(event.name + event.killed)
-       }
+        if (names.includes(event.name)) {
+            sendMessage(event.name)
+        }
     })
 
     function sendMessage(name) {
         setTimeout(function () {
-            dispatch.toServer('C_CHAT', 1, {channel: 1, message: name + messages[rand(messages.length)]});
+            dispatch.toServer('C_CHAT', 1, {channel: 1, message: name  + messages[rand(messages.length)]});
         }, 500);
     }
 
