a6723c9ac70fd33b35ab25b8ee8600ea3958c730
Modnox <marco.feder@gmx.de>
2021-03-08 18:47:41 +0100

    added new messages

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ module.exports = function pepega(dispatch) {
             '. Here we go again!',
             ' PEPEGA!',
             ' nice floormat.',
-            ' is getting closer to the wheelchair.'
+            ' is getting closer to the wheelchair.',
+            '. /drop when?',
+            ' is taking a break.',
+            '. Nice parse!',
+            ' is living the Youtuber lifestyle.',
+            '.exe has stopped working.',
+            '! Guide has been disabled.',
+            '. Mistakes were made.'
         ]
 
     dispatch.command.add('pepega', (cmd) => {
