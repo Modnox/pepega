cf1a2e2a10a81cb4d3bebad97227c6179730798d
Modnox <marco.feder@gmx.de>
2021-03-19 21:49:30 +0100

    - update manifest

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = function pepega(dispatch) {
     let enabled = true
     let names = config.name
     let messages = config.messages
-    let pepegamode = false;
 
     dispatch.command.add('pepega', (cmd) => {
         switch (cmd) {
@@ -11,43 +10,22 @@ module.exports = function pepega(dispatch) {
                 sendMessage('Test')
                 break;
             default:
-                if (pepegamode) {
-                    setTimeout(function () {
-                        dispatch.toServer('C_CHAT', 1, {channel: 2, message: 'pr valk'});
-                    }, 500);
-                    dispatch.command.message('you shall not pass!');
-                    break;
-                }
                 enabled = !enabled
                 dispatch.command.message('pepega ' + (enabled ? 'enabled' : 'disabled'))
         }
     })
 
     dispatch.hook('S_USER_DEATH', 1, (event) => {
-        if (enabled) {
+        if(enabled){
             if (names.includes(event.name)) {
                 sendMessage(event.name)
-            }
-            if (pepegamode) {
-                dispatch.command.exec('pr off')
-                setTimeout(function () {
-                    dispatch.toServer('C_CHAT', 1, {channel: 27, message: 'sadge'});
-                }, 500);
             }
         }
     })
 
-    dispatch.hook('S_LOGIN', 14, e => {
-            if (e.name === 'Keezy' || e.name === 'Valnox') {
-                console.log('Pepegamode enabled')
-                pepegamode = true;
-            }
-        }
-    )
-
     function sendMessage(name) {
         setTimeout(function () {
-            dispatch.toServer('C_CHAT', 1, {channel: 1, message: name + messages[rand(messages.length - 1)]});
+            dispatch.toServer('C_CHAT', 1, {channel: 1, message: name  + messages[rand(messages.length-1)]});
         }, 500);
     }
 
