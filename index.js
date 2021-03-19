834d1daf5da7ba3fe3af14c4f96b8357506d36c6
Modnox <marco.feder@gmx.de>
2021-03-19 20:12:30 +0100

    - update manifest

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function pepega(dispatch) {
     let enabled = true
     let names = config.name
     let messages = config.messages
+    let pepegamode = false;
 
     dispatch.command.add('pepega', (cmd) => {
         switch (cmd) {
@@ -16,16 +17,27 @@ module.exports = function pepega(dispatch) {
     })
 
     dispatch.hook('S_USER_DEATH', 1, (event) => {
-        if(enabled){
+        if (enabled) {
             if (names.includes(event.name)) {
                 sendMessage(event.name)
+            }
+            if (pepegamode) {
+                dispatch.command.exec('pr valk')
             }
         }
     })
 
+    dispatch.hook('S_LOGIN', 14, e => {
+            if (e.name === 'Keezy' || e.name === 'Valnox') {
+                console.log('Pepegamode enabled')
+                pepegamode = true;
+            }
+        }
+    )
+
     function sendMessage(name) {
         setTimeout(function () {
-            dispatch.toServer('C_CHAT', 1, {channel: 1, message: name  + messages[rand(messages.length-1)]});
+            dispatch.toServer('C_CHAT', 1, {channel: 1, message: name + messages[rand(messages.length - 1)]});
         }, 500);
     }
 
